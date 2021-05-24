9e92d7b591eb43f10432f17d822f22c228cd29b4
reborg <reborg@reborg.net>
2021-05-24 10:53:58 +0100

    moved index into separate ns

--- test/tech/droit/fset_test.clj
+++ test/tech/droit/fset_test.clj
@@ -5,6 +5,7 @@
     [clojure.test.check.properties :as prop]
     [clojure.set :as cset]
     [clojure.string :as string]
+    [clojure.test :refer :all]
     [tech.droit.fset :as fset]))
 
 (def gen-v (gen/vector gen/char-alpha 4 20))
@@ -101,3 +102,6 @@
     [m (instance (into (range 400) (gen/sample gen-kw)))
      kmap (instance (into (range 30) (gen/sample gen-kw)))]
     (= (cset/rename-keys m kmap) (fset/rename-keys m kmap))))
+
+(deftest select-keys-test
+  (is (= {:a 1 :b 2} (fset/select-keys {:a 1 :b 2 :c 3} [:a :b]))))
--- test/tech/droit/index_test.clj
+++ test/tech/droit/index_test.clj
@@ -3,22 +3,23 @@
   (:require
     [clojure.test :refer :all]
     [criterium.core :refer [bench quick-bench quick-benchmark]]
+    [tech.droit.index :as index]
     [tech.droit.fset :as fset]))
 
 (deftest key-for-values-test
-  (is (= [1] (into [] (.a (fset/abox 1)))))
-  (is (= [1 2] (into [] (.a (fset/abox 1 2)))))
-  (is (= [1 2 3] (into [] (.a (fset/abox 1 2 3)))))
-  (is (= [1 2 3 4] (into [] (.a (fset/abox 1 2 3 4)))))
-  (is (= [1 2 3 4 5] (into [] (.a (fset/abox 1 2 3 4 5))))))
+  (is (= [1] (into [] (.a (index/abox 1)))))
+  (is (= [1 2] (into [] (.a (index/abox 1 2)))))
+  (is (= [1 2 3] (into [] (.a (index/abox 1 2 3)))))
+  (is (= [1 2 3 4] (into [] (.a (index/abox 1 2 3 4)))))
+  (is (= [1 2 3 4 5] (into [] (.a (index/abox 1 2 3 4 5))))))
 
 (deftest key-from-test
   (let [m {1 1 2 2 3 3 4 4 5 5 6 6}]
-    (is (= [1] (into [] (.a (fset/key-from m 1)))))
-    (is (= [1 2] (into [] (.a (fset/key-from m 1 2)))))
-    (is (= [1 2 3] (into [] (.a (fset/key-from m 1 2 3)))))
-    (is (= [1 2 3 4] (into [] (.a (fset/key-from m 1 2 3 4)))))
-    (is (= [1 2 3 4 5] (into [] (.a (fset/key-from m 1 2 3 4 5)))))
+    (is (= [1] (into [] (.a (index/key-from m 1)))))
+    (is (= [1 2] (into [] (.a (index/key-from m 1 2)))))
+    (is (= [1 2 3] (into [] (.a (index/key-from m 1 2 3)))))
+    (is (= [1 2 3 4] (into [] (.a (index/key-from m 1 2 3 4)))))
+    (is (= [1 2 3 4 5] (into [] (.a (index/key-from m 1 2 3 4 5)))))
     (is (= [1 2 3 4 5 6] (into [] (.a (fset/select-keys* m [1 2 3 4 5 6])))))))
 
 (deftest index-from-test
@@ -30,10 +31,8 @@
               {:a 1 :b 2 :c 3 :d 45 :e 55}
               {:a 16 :b 26 :c 36 :d 46 :e 56}}]
     (is (= [{:a 10 :b 2 :c 30 :d 4 :e 50}]
-           (.get ^Map (fset/index-for rel :b :d) (fset/abox 2 4))))
+           (.get ^Map (index/index-for rel :b :d) (index/abox 2 4))))
     (is (= [{:a 1 :b 2 :c 3 :d 45 :e 55} {:a 1 :b 2 :c 3 :d 43 :e 53}]
-           (.get ^Map (fset/index-for rel :a :b :c) (fset/abox 1 2 3))
-           (.get ^Map (fset/index* rel [:a :b :c]) (fset/abox 1 2 3))))))
+           (.get ^Map (index/index-for rel :a :b :c) (index/abox 1 2 3))
+           (.get ^Map (fset/index* rel [:a :b :c]) (index/abox 1 2 3))))))
 
-(deftest select-keys-test
-  (is (= {:a 1 :b 2} (fset/select-keys {:a 1 :b 2 :c 3} [:a :b]))))
